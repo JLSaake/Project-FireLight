b4ba474e53a45e50ff3ab4adc8750f8027fdfa7a
John Luke Saake <johnlukesaake@gmail.com>
2019-08-01 13:44:31 -0700

    AI Stationary Check
    Enemy now checks to see if it is stationary after losing sight of the player, waits for specified time, then returns to starting position.
    TODO: make return to nearest sentry mode node.

--- Project FireLight/Assets/Scripts/Enemy.cs
+++ Project FireLight/Assets/Scripts/Enemy.cs
@@ -5,23 +5,31 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-
+    #region Variables
     private NavMeshAgent agent; // NavMesh agent found in Start on Enemy
     private Player player; // TODO: get from gamemanager
     private bool isFollowingPlayer = false; // If this enemy is currently tracking the player
     private bool isAttacking = false; // If this enemy is currently making an attack
+    private bool isStationary = false; // If this enemy is currently stationary
+    private bool isStationaryCounting = false; // If this enemy is currently running stationary time counter
+    private float stationaryTimeStart = 0; // Amount of time this enemy has remained stationary
+    public float stationaryTimeLimit = 2; // Amount of time this enemy should remain stationary after losing sight of player
     public float attackDistance = 2f;
     public int health = 1; // HP pool. Below 0 = Death
     public float detectionDistance = 10; // Normal distance to detect player directly in front
     public float limitedDetectionDistance = 5; // Shortened distance to detect player in peripherial vision
     public float forwardDetectionAngle = 45; // Angle from forward at which normal detection distance applies
     public float sideDetectionAngle = 90; // Angle from forward at which shortened detection distance applies
+    #endregion
+
+    private Vector3 startPos;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindObjectOfType<Player>();
+        startPos = agent.transform.position;
     }
 
     // Update is called once per frame
@@ -32,8 +40,15 @@ public class Enemy : MonoBehaviour
             Die();
         }
         bool seePlayer = CanSeePlayerInRange();
-        if (!isAttacking && seePlayer) // Design question: Do we store player's last known position while attacking?
+        
+        if (!isAttacking && isFollowingPlayer) {
+            isStationary = StationaryCheck();
+        }
+
+        if (seePlayer && !isAttacking) // Design question: Do we store player's last known position while attacking?
         {
+            isStationary = false;
+            isStationaryCounting = false;
             agent.SetDestination(player.GetPlayerPosition());
             isFollowingPlayer = true;
             
@@ -44,16 +59,19 @@ public class Enemy : MonoBehaviour
                 StartCoroutine("Attack");
             }
         } else
-        if (!seePlayer && isFollowingPlayer && !isAttacking)
-        {
-            if (agent.remainingDistance <= 1) // close enough to point
-            {
-                isFollowingPlayer = false;
-                // TODO add stationary check and sentry mode return
-                agent.SetDestination(agent.transform.position);
+        if (!seePlayer && isStationary && isFollowingPlayer) {
+            if (isStationaryCounting) { // If stationary timer is running
+                if ((Time.time - stationaryTimeStart) >= stationaryTimeLimit) { // If the stationary time is over the limit
+                    isFollowingPlayer = false;
+                    // TODO: return to nearest sentry point
+                    agent.SetDestination(startPos); // Temporary: return to starting location
+                }
+            } else { // Start timer stationary timer
+                isStationaryCounting = true;
+                stationaryTimeStart = Time.time;
             }
         }
-
+        Debug.Log(stationaryTimeStart);
     }
 
     // Checks to see if the player can be seen, and is range based on sight angles
@@ -94,6 +112,10 @@ public class Enemy : MonoBehaviour
         if (health <= 0) {
             Die();
         }
+    }
+
+    bool StationaryCheck() {
+      return Vector3.Distance(agent.transform.position, agent.destination) <= .1; // TODO: Add stationary tolerance variable
     }
 
     IEnumerator Attack() {
