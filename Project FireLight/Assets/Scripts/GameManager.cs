324617de9364576c85a370132ecb326c6b30a515
John Luke Saake <johnlukesaake@gmail.com>
2019-08-05 11:04:23 -0700

    Overhead Room Blockers
    Added dark blockers that obscure future rooms from the player, and are cleared by entering the room.

--- Project FireLight/Assets/Scripts/GameManager.cs
+++ Project FireLight/Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     Player player; // Found in Start
     public Camera overheadCam; // TODO: get main camera via script and tags
     private Vector3 cameraOffset; // Set at Start, is the offset of the camera from player
+    public GameObject overheadBlockers;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,10 @@ public class GameManager : MonoBehaviour
         player.SetCam(overheadCam);
         Vector3 playerPos = player.GetPlayerPosition();
         cameraOffset = overheadCam.transform.position - playerPos;
+        if (overheadBlockers)
+        {
+            overheadBlockers.gameObject.SetActive(true);
+        }
     }
 
     // Update is called once per frame
---
+++ Project FireLight/Assets/Scripts/OverheadBlocker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverheadBlocker : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
