dd8cab52af4b1cd7049ddbe1fe844f0f2bdcdc8d
John Luke Saake <johnlukesaake@gmail.com>
2019-08-05 09:44:35 -0700

    Working Doors
    Doors now divide each room and prevent enemies from following the player into the next room.

---
+++ Project FireLight/Assets/Scripts/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public float cooldown = 2f;
+    private BoxCollider boxCollider;
+    private MeshRenderer meshRenderer;
+
+    void Start()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider>();
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            StartCoroutine("ToggleDoor");
+        }
+    }
+
+    IEnumerator ToggleDoor() {
+        boxCollider.enabled = false;
+        meshRenderer.enabled = false;
+        yield return new WaitForSeconds(cooldown);
+        boxCollider.enabled = true;
+        meshRenderer.enabled = true;
+    }
+}
